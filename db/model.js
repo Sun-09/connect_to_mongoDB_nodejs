2868bed534a7f85a7b1c4092b782fe69e4685bd1
Souradeep <souradeepbera162@gmail.com>
2023-04-21 13:25:49 +0530

    "new commit"

--- db/model.js
+++ db/model.js
@@ -2,11 +2,46 @@ const mongoose = require("mongoose");
 
 
 const testSchema = new mongoose.Schema({
-    result :{
+    0 :{
+        type:String,
+        required:true
+    },
+    1 :{
+        type:String,
+        required:true
+    },
+    2 :{
+        type:String,
+        required:true
+    },
+    3 :{
+        type:String,
+        required:true
+    },
+    4 :{
+        type:String,
+        required:true
+    },
+    5 :{
+        type:String,
+        required:true
+    },
+    6 :{
+        type:String,
+        required:true
+    },
+    7 :{
+        type:String,
+        required:true
+    },
+    8 :{
+        type:String,
+        required:true
+    },
+    9 :{
         type:String,
         required:true
     }
-    
 })
 
 // create collection
